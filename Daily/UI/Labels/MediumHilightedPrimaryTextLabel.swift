3b60bfa0223f0a5ac8b60d1598faca53dd5d6c20
Никифоров Василий Петрович <vpnikiforov@domclick.ru>
2020-02-24 08:57:41 +0300

    9

--- Daily/UI/Labels/MediumHilightedPrimaryTextLabel.swift
+++ Daily/UI/Labels/MediumHilightedPrimaryTextLabel.swift
@@ -2,7 +2,7 @@ import UIKit
 
 final class LargeMediumBoldPrimaryTextLabel: BaseLabel {
     override func setSettings() {
-        font = UIFont.boldSystemFont(ofSize: 24)
+        font = UIFont.boldSystemFont(ofSize: 22)
         textColor = Theme.primaryTextColor
     }
 }
--- Daily/Views/CalendarActiveDayCollectionVIewCell/CalendarActiveDayCollectionVIewCell.swift
+++ Daily/Views/CalendarActiveDayCollectionVIewCell/CalendarActiveDayCollectionVIewCell.swift
@@ -11,12 +11,34 @@ class CalendarActiveDayCollectionVIewCell: UICollectionViewCell {
     @IBOutlet private weak var highlightedDateLabel: LargeMediumBoldPrimaryTextLabel!
     @IBOutlet private weak var circleView: UIView!
     
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
+//    private var circle: CAShapeLayer? = nil
+//
+//    override func layoutSubviews() {
+//        super.layoutSubviews()
+//
+//        print(circleView.frame)
+//    }
 
     func configure(_ viewModel: CalendarActiveDayViewModel) {
+//        if circle == nil {
+//            let edge = min(circleView.frame.width, circleView.frame.height) / 2
+//            let circlePath = UIBezierPath(arcCenter: CGPoint(x: edge, y: edge),
+//                                          radius: CGFloat(20),
+//                                          startAngle: CGFloat(0),
+//                                          endAngle: CGFloat(Double.pi * 2),
+//                                          clockwise: true)
+//
+//            circle = CAShapeLayer()
+//
+//            if let circle = circle {
+//                circle.path = circlePath.cgPath
+//                circleView.layer.addSublayer(circle)
+//            }
+//        }
+//        circle?.fillColor = viewModel.dayColor.cgColor
+        
+        circleView.backgroundColor = viewModel.dayColor
+        
         if viewModel.isHighlited {
             dateLabel.text = nil
             highlightedDateLabel.text = viewModel.date.dateNumber
@@ -24,7 +46,5 @@ class CalendarActiveDayCollectionVIewCell: UICollectionViewCell {
             dateLabel.text = viewModel.date.dateNumber
             highlightedDateLabel.text = nil
         }
-        
-        circleView.backgroundColor = viewModel.dayColor
     }
 }
