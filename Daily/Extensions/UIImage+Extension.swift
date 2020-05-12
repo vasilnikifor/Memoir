bbe46e66657560480fa00f55846b0c9f11f3cf61
Никифоров Василий Петрович <vpnikiforov@domclick.ru>
2020-05-12 11:58:25 +0300

    Change image quality

--- Daily/Extensions/UIImage+Extension.swift
+++ Daily/Extensions/UIImage+Extension.swift
@@ -1,8 +1,8 @@
 import UIKit
 
 extension UIImage {
-    func getCompressedData(to sizeInMb: Int = 2) -> Data? {
-        let requiredSizeInBytes = sizeInMb * 1024 * 1024
+    func getCompressedData(to sizeInMb: Int = 1) -> Data? {
+        let requiredSizeInBytes = Int(sizeInMb * 1024 * 1024)
         let compressionsQuality: [CGFloat] = [1, 0.75, 0.5, 0.25, 0.1, 0]
         
         for compressionQualityItem in compressionsQuality {
